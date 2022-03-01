5836a52ecde6de7de970d8e309100f9bd377e67d
Sandberg, Nathan <Nathan.Sandberg@cdk.com>
2022-03-01 16:09:47 -0600

    First Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import time, sys
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+import json
+
+import requests
+
+class EventHandler(FileSystemEventHandler):
+    print("Handler Called")
+    def on_modified(self, event):
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+        with open('payload.json', 'r') as f:
+            lines = f.readlines()
+            print(json.dumps(lines, indent=4))
+            response = requests.post(
+                '{yourWebhookURL}',
+                data = json.dumps(lines, indent=4),
+                headers= {
+                    'Accept':'application/json',
+                    'Cache-Control':'no-cache'
+                    }
+                )
+            print(response)
+
+src_path = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+event_handler=EventHandler()
+observer = Observer()
+observer.schedule(event_handler, path=src_path, recursive=True)
+print("Monitoring started")
+observer.start()
+try:
+    while(True):
+        print('timer', flush=True)
+        time.sleep(10 )
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
